88c66e4d105b3123eb43ffd7932e76d1b98a43e7
Reza Mirzapour <mirzaandrampage@gmail.com>
2025-07-29 09:06:53 +0330

    feat(Q00): add some other questions

--- 00.ts
+++ 00.ts
@@ -1,8 +1,21 @@
-// Q0: Let's talk a bit about below tools
+// Q0: Let's talk a bit about below sentences
 
-// Nextjs v15 (App router - dynamic routes - apit routes - server actions - group routes - SEO - Cach - Revalidate Cache)
+// Nextjs v15 (App router - dynamic routes - group routes - api routes - server actions - SEO - Cach - Revalidate Cache)
+// what is the main diffrence in file structure between App Router and Page Router
+// what is a dynamic route? explain the file and folder structure
+// what is a group route? explain the file and folder structure
+// what is an api route? what's the diffrence of an api route and a server action?
+// in page router, what's the diffrence between getStaticProps and getServerSideProps?
+// which built-in functions are used to revalidate caches in next js?
+// which meta tags do you know which are used to enhance SEO?
+// which functions in nextjs you know which are using to enhance seo?
+
 // Tailwindcss
+
+// Basic Git (commit - push - pull - merge - resolve confilicts - pipelines)
+
 // React Query
+
 // Form Tools Like RHF, Formik, Zod and Yup
 
 // Have you ever implemented a complex project? not routin once!
